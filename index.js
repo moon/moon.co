a24ca012f6ef66b2ea954f32ba2ad93d8281e115
Matthew Wagerfield <matthew@wagerfield.com>
2015-06-01 23:46:49 +0100

    Updated core files

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
 (function() {
 
-    var el = document.querySelector('.space');
-    var bg = new Image();
-    
-    bg.onload = function() {
-        el.style.backgroundImage = 'url(' + bg.src + ')';
-        el.className += ' animate';
-    };
-
-    bg.src = 'nebula.jpg';
-
 })();
