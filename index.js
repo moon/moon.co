5fafb1d016406b4940c073abba766c6031dbda91
Matthew Wagerfield <matthew@wagerfield.com>
2015-06-19 18:33:57 +0100

    Update

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 (function() {
 
+  var url = 'http://moonlander.meteor.com/';
+  var frame = document.querySelector('.frame');
+  var iframe = document.createElement('iframe');
+
+  frame.appendChild(iframe);
+
+  iframe.onload = function(event) {
+
+    ga('send', 'event', 'embed', 'loaded', url);
+
+    frame.classList.add('show');
+
+  };
+
+  ga('send', 'event', 'embed', 'load', url);
+
+  iframe.src = url;
+
 })();
